b9e6f73d013c9f8e0851de633377a15d27b4230d
hjs2027864933 <2027864933@qq.com>
2020-12-07 20:48:37 +0800

    修改一些bug

--- src/main/java/com/example/chatonline/ChatOnlineApplication.java
+++ src/main/java/com/example/chatonline/ChatOnlineApplication.java
@@ -3,15 +3,13 @@ package com.example.chatonline;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @MapperScan("com.example.chatonline.Dao") //扫描的mapper
 @SpringBootApplication
-public class JwtxApplication {
+public class ChatOnlineApplication {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(JwtxApplication.class, args);
+        SpringApplication.run(ChatOnlineApplication.class, args);
     }
 
 }
