f50f48b24409528e127d3d345607e59db6faf652
z@wood <2511841570@qq.com>
2020-12-08 16:29:47 +0800

    :lipstick: 完成查找UI

--- front-end/src/views/main/index.tsx
+++ front-end/src/views/main/index.tsx
@@ -1,27 +1,50 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import './index.scss'
 import FriendContent from './friendContent/index';
 import Message from './message/index'
 import { Container } from './store/index'
-
+import AddFriend from './addFriend/index'
 
 const Main: React.FC = () => {
+  const [select, setSelect] = useState<string>("me")
   return (
     <div className="main">
       <div className="main-container">
         <div className="aside">
           <div className="avater">
-            <img src={require('../../assets/imgs/avater.svg')} />
+            <img onClick={() => {setSelect("me")}} src={require('../../assets/imgs/avater.svg')} />
           </div>
           <div>
-            <img className="me" src={require('../../assets/imgs/me.svg')} />
+            <img className="me" onClick={() => {setSelect("add")}} src={require('../../assets/imgs/me.svg')} />
           </div>
         </div>
         <div className="main-body">
-          <Container>
-            <FriendContent />
-            <Message />
-          </Container>
+          {
+            (() => {
+              switch(select) {
+                case "me":
+                  return (
+                    <Container>
+                      <FriendContent />
+                      <Message />
+                    </Container>
+                  )
+                case "add":
+                  return (
+                    <Container>
+                      <AddFriend />
+                    </Container>
+                  )
+                default:
+                  return (
+                    <Container>
+                      <FriendContent />
+                      <Message />
+                    </Container>
+                  )
+              }
+            })()
+          }
         </div>
       </div>
     </div>
