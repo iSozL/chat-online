6533917050e35e3b37568ecc8ba82cf466f234d6
z@wood <2511841570@qq.com>
2020-12-08 21:33:22 +0800

    :sparkles: 简单完成 websocket

--- front-end/src/views/main/message/index.tsx
+++ front-end/src/views/main/message/index.tsx
@@ -1,15 +1,70 @@
-import React, { useContext, useRef } from 'react';
-import {changeUserContext} from '../store/index'
+import React, { useContext, useRef, useEffect } from 'react';
+import {changeUserContext, CHANGE_USER} from '../store/index'
 import './index.scss'
 import { Button, Popover } from 'antd'
-// const { TextArea } = Input
 import Scroll from 'react-custom-scrollbars'
 import 'emoji-mart/css/emoji-mart.css'
 import { Picker } from 'emoji-mart'
 
+let client: string;
+let socket: any;
+if (!window.WebSocket) {
+  window.WebSocket = window.MozWebSocket;
+}
+if (window.WebSocket) {
+  socket = new WebSocket("ws://localhost:8081/ws");
+} else {
+  alert("你的浏览器不支持 WebSocket！");
+}
 const Message = () => {
   const { userMsg, useDispatch } = useContext(changeUserContext)
+
+  socket.onmessage = function (event: any) {
+    event = JSON.parse(event.data)
+    console.log(event)
+    if (!event.init) {
+      client = event.client
+      return
+    }
+    if (event.client === client) {
+      let data = {
+        message: event.message,
+        time: new Date(),
+        my: true
+      }
+      useDispatch({type: CHANGE_USER, state:{username: '孙笑川', show: true, msgs: userMsg.msgs.concat(data)}})
+    } else {
+      let data = {
+        message: event.message,
+        time: new Date(),
+        my: false
+      }
+      useDispatch({type: CHANGE_USER, state:{username: '孙笑川', show: true, msgs: userMsg.msgs.concat(data)}})
+    }
+  };
+  socket.onopen = function (event: any) {
+    console.log("连接开始")
+  };
+  socket.onclose = function (event: any) {
+    console.log("连接关闭")
+  };
+
+  
+
   const inputs = useRef<any>()
+
+  const send = (message: any) => {
+    if (!window.WebSocket) {
+      return;
+    }
+    if (socket.readyState == WebSocket.OPEN) {
+      socket.send(message);
+      inputs.current.value = ""
+    } else {
+      alert("连接没有开启.");
+    }
+  }
+
   const emoji = (
     <div>
       <Picker onSelect={(emoji: any)=> {inputs.current.value += emoji.native}} />
@@ -22,34 +77,32 @@ const Message = () => {
         <>
         <div className="msg-body">
           <div className="msg-header">
-            {userMsg.username}
           </div>
           <div>
-            <Scroll style={{height: "45vh", width: "99%", display: "flex"}}>
-              <div style={{padding: "10px 30px"}}>
-                <img style={{width: "50px"}} src={require('../../../assets/imgs/avater.svg')} />
-                <div className="arrow-box popper border arrow-left">
-                  我徒弟呢？
-                </div>
-              </div>
-              <div style={{padding: "10px 30px", flex: 1, textAlign: "right"}}>
-                <div className="arrow-box popper border arrow-right">
-                  我不到啊
-                </div>
-                <img style={{width: "50px"}} src={require('../../../assets/imgs/avater.svg')} />
-              </div>
-              <div style={{padding: "10px 30px"}}>
-                <img style={{width: "50px"}} src={require('../../../assets/imgs/avater.svg')} />
-                <div className="arrow-box popper border arrow-left">
-                  我徒弟呢？
-                </div>
-              </div>
-              <div style={{padding: "10px 30px", flex: 1, textAlign: "right"}}>
-                <div className="arrow-box popper border arrow-right">
-                  我不到啊
-                </div>
-                <img style={{width: "50px"}} src={require('../../../assets/imgs/avater.svg')} />
-              </div>
+            <Scroll style={{height: "48vh", width: "99%", display: "flex"}}>
+              {
+                userMsg.msgs.map((item: any, index: number) => {
+                  if (!item.my) {
+                    return (
+                      <div style={{padding: "10px 30px"}} key={index}>
+                        <img style={{width: "50px"}} src={require('../../../assets/imgs/avater.svg')} />
+                        <div className="arrow-box popper border arrow-left">
+                          {item.message}
+                        </div>
+                      </div>
+                    )
+                  } else {
+                    return (
+                      <div style={{padding: "10px 30px", flex: 1, textAlign: "right"}} key={index}>
+                        <div className="arrow-box popper border arrow-right">
+                          {item.message}
+                        </div>
+                        <img style={{width: "50px"}} src={require('../../../assets/imgs/avater.svg')} />
+                      </div>
+                    )
+                  }
+                })
+              }
             </Scroll>
           </div>
         </div>
@@ -61,7 +114,7 @@ const Message = () => {
           </div>
           <div className="footer-body">
             <textarea className="ant-input" ref={inputs} />
-            <Button style={{float: 'right', marginTop: "10px"}} type="primary">发送</Button>
+            <Button style={{float: 'right', marginTop: "10px"}} type="primary" onClick={() =>send(inputs.current.value)}>发送</Button>
           </div>
           <div className="footer-bottom">
           </div>
