bec309208f2a5d02d3d861b4254755c6784da9d6
z@wood <2511841570@qq.com>
2020-12-08 12:33:15 +0800

    :lipstick: 完成消息详细界面

--- front-end/src/views/main/message/index.tsx
+++ front-end/src/views/main/message/index.tsx
@@ -1,20 +1,35 @@
 import React, {useContext} from 'react';
 import {changeUserContext} from '../store/index'
 import './index.scss'
+import { Button, Input } from 'antd'
+const { TextArea } = Input
+
 const Message = () => {
   const { userMsg, useDispatch } = useContext(changeUserContext)
   return (
     <div className="msg-container">
       {
         userMsg.show ? 
+        <>
         <div className="msg-body">
           <div className="msg-header">
             {userMsg.username}
+          </div>  
+        </div>
+        <div className="msg-footer">
+          <div className="footer-top">
+            <img style={{width: "30px", height: "30px"}} src={require('../../../assets/imgs/face.svg')}></img>
           </div>
-        </div> :
+          <div className="footer-body">
+            <TextArea rows={6} />
+            <Button style={{float: 'right', marginTop: "10px"}} type="primary">发送</Button>
+          </div>
+          <div className="footer-bottom">
+          </div>
+        </div>
+      </> :
         <img className="none-msg" src={require('../../../assets/imgs/msg-bg.svg')} />
       }
-      <div className="msg-footer"></div>
     </div>
   )
 }
