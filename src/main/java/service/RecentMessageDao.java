720e420692e3b3d6668a4c265ddb868c30efd5b0
cyw <1874789886@qq.com>
2020-12-20 19:53:20 +0800

    实时修改最新消息列表

---
+++ src/main/java/service/RecentMessageDao.java
@@ -0,0 +1,7 @@
+package service;
+
+public interface RecentMessageDao {
+    boolean storingMessage(String sendId,String receiveId,String sendtime,String messagetxt);
+    boolean isExists(String sendId,String receiveId);
+    boolean updateMessage(String sendId,String receiveId,String sendtime,String messagetxt);
+}
