befea804b9731b30fa769b20c8e99f3960aa6785
z@wood <2511841570@qq.com>
2020-12-22 10:22:59 +0800

    :lipstick: 完善好友印象 UI

--- front-end/src/views/main/commets/index.tsx
+++ front-end/src/views/main/commets/index.tsx
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import './index.scss'
-import {message} from 'antd'
+import {message, Popconfirm} from 'antd'
 import request from '../../../utils/request'
+import Scroll from 'react-custom-scrollbars'
 const Comments = () => {
-  const [commentList, setList] = useState()
-  async function getMes(id: string) {
-    await request.get(`http://101.132.134.186:8080/ShowFriendImage?friendId=${id}`).then(async value => {
+  let info = JSON.parse(window.localStorage.getItem("userInfo"))
+  const [commentList, setList] = useState<any>()
+  async function getMes() {
+    await request.get(`http://101.132.134.186:8080/ShowImage?userId=${info.userId}`).then(async value => {
       if (value.data.code) {
         setList(value.data.data)
       } else {
@@ -13,12 +15,52 @@ const Comments = () => {
       }
     })
   }
+  async function confirmDel(msg: any) {
+    await request.get(`http://101.132.134.186:8080/DelReceiveImage?userId=${info.userId}&friendId=${msg.userId}&time=${msg.sendtime}`).then(async value => {
+      if (value.data.code) {
+        message.success(value.data.message)
+        console.log(value.data.data)
+      } else {
+        message.error(value.data.message)
+      }
+    })
+  }
   useEffect(() => {
-    getMes
+    getMes()
   }, [])
   return (
     <div className="comment-container">
-      留言
+      <div className="comments-body">
+        <Scroll style={{width: "600px", wordBreak: "break-all", background: '#d5d8db', borderRadius: "10px"}}>
+          <div style={{margin: "30px 0 10px 0"}}>
+            {
+              commentList && commentList instanceof Array ?
+              commentList.map((item: any, index: number) => {
+                return (
+                  <div className="comment" key={index}>
+                    <div>
+                      {item.message}
+                    </div>
+                    <div>{item.sendtime}</div>
+                    <div style={{display: "flex", justifyContent: "space-between"}}>
+                      <span>{item.userId}</span>
+                      <Popconfirm
+                        title="确认删除这条留言吗?"
+                        onConfirm={() => confirmDel(item)}
+                        okText="确认"
+                        cancelText="取消"
+                      >
+                        <a href="#" className="delete">删除</a>
+                      </Popconfirm>
+                    </div>
+                  </div>
+                )
+              }) :
+              "暂无好友印象"
+            }
+          </div>
+        </Scroll>
+      </div>
     </div>
   )
 }
